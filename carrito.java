cf0db35b3cd3472cc08e2b8f5bf6b70fe18ee3c0
tony18121993 <131663809+tony18121993@users.noreply.github.com>
2023-05-08 12:10:24 +0200

    Version 2:añadido cliente y modificacion de carrito

--- carrito.java
+++ carrito.java
@@ -1,5 +1,9 @@
 class carrito {
 	float total;
 	long id;
+	
+	carrito(long id){
+	this.id=id;
+	}
 
 }
---
+++ cliente.java
@@ -0,0 +1,12 @@
+clas cliente{
+
+	string dni;
+	string nombre;
+	
+	cliente(String dni,string nombre){
+	this.dni=dni;
+	this.nombre=nombre;
+	
+	}
+
+}
